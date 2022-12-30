8b7128a9580f7c0ccde5ed4bcce293de4c01a3b6
sinhasnowflake <121646579+sinhasnowflake@users.noreply.github.com>
2022-12-29 23:54:23 -0800

    Update streamlit_app.py
    set fruit index

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('almond flour pancake');
 streamlit.text('almond flour waffle with kale and almond');
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
