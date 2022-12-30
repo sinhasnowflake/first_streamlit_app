1a179f2b23196f71316f83c85c68d9c3dc6d3082
sinhasnowflake <121646579+sinhasnowflake@users.noreply.github.com>
2022-12-29 23:50:15 -0800

    Update streamlit_app.py
    imported pandas and displayed dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
 streamlit.title('My parents healthy diner');
 streamlit.header('Breakfast Menu');
 streamlit.text('almond flour pancake');
 streamlit.text('almond flour waffle with kale and almond');
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
