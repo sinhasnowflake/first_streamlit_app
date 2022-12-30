e2d9e4c8ad35413918a776f4aa71019c5bf4693a
sinhasnowflake <121646579+sinhasnowflake@users.noreply.github.com>
2022-12-30 10:59:59 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.header('Breakfast Menu');
 streamlit.text('almond flour pancake');
 streamlit.text('almond flour waffle with kale and almond');
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.dataframe(my_fruit_list)
+
 
