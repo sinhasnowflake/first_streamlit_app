0db1128af9fa1e1b4c7a71d33034ba5a04f759ff
sinhasnowflake <121646579+sinhasnowflake@users.noreply.github.com>
2022-12-29 18:15:15 -0800

    Update streamlit_app.py
    updated breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My parents healthy diner');
+streamlit.header('Breakfast Menu');
+streamlit.text('almond flour pancake');
+streamlit.text('almond flour waffle with kale and almond');
