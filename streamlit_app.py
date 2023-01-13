26b216ea24d180c6db6ce0b0842597b78d144503
sinhasnowflake <121646579+sinhasnowflake@users.noreply.github.com>
2023-01-12 22:38:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 import pandas
 streamlit.title('My parents healthy diner');
 streamlit.header('Breakfast Menu');
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 streamlit.text('almond flour pancake');
 streamlit.text('almond flour waffle with kale and almond');
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
